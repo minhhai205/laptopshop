168aecc978551fae4010277319641f57c077efb0
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-12-10 00:01:54 +0700

    applying the MVC model

---
+++ src/main/java/vn/minhhai/laptopshop/controller/UserController.java
@@ -0,0 +1,31 @@
+package vn.minhhai.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.minhhai.laptopshop.service.UserService;
+
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "hello from controller";
+//     }
+// }
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
---
+++ src/main/java/vn/minhhai/laptopshop/service/UserService.java
@@ -0,0 +1,10 @@
+package vn.minhhai.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello() {
+        return "Hello from service";
+    }
+}
