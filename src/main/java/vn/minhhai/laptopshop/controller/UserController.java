b38ddbf987eac2d3df1a8e5b0c034cdb5929b8e0
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-12-11 16:35:27 +0700

    create user interface

--- src/main/java/vn/minhhai/laptopshop/controller/UserController.java
+++ src/main/java/vn/minhhai/laptopshop/controller/UserController.java
@@ -2,11 +2,10 @@ package vn.minhhai.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import vn.minhhai.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -22,18 +21,10 @@ public class UserController {
         model.addAttribute("data", data);
         return "hello";
     }
+
+    @RequestMapping("admin/user/create")
+    public String requestMethodName() {
+        return "/admin/user/create";
+    }
+
 }
-
-// @RestController
-// public class UserController {
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("/")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-// }
