7b3cf40b10da02ed8bac41f3e0ca370587e8647e
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2025-02-03 21:12:07 +0700

    render all product client

--- src/main/java/vn/minhhai/laptopshop/controller/client/ProductClientController.java
+++ src/main/java/vn/minhhai/laptopshop/controller/client/ProductClientController.java
@@ -1,12 +1,26 @@
 package vn.minhhai.laptopshop.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import vn.minhhai.laptopshop.domain.Product;
+import vn.minhhai.laptopshop.service.ProductService;
 
 @Controller
 public class ProductClientController {
+    private final ProductService productService;
+
+    public ProductClientController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product")
-    public String getProductPage() {
+    public String getProductPage(Model model) {
+        List<Product> products = this.productService.getAllProducts();
+        model.addAttribute("products", products);
         return "client/product/index";
     }
 
